39c23b5f6b01f95979ea44672347735be5717be6
Thewizard18n <gustavohenriquedoespirito@gmail.com>
2025-10-15 00:27:16 -0300

    retirando import

--- filters/CustomAuthorizationFilter.java
+++ filters/CustomAuthorizationFilter.java
@@ -2,7 +2,6 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import com.finance.api.LoggedUser;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
